7b0e012f6c4b09852cd89bc1d6ef3f5897f9c386
Numilen <numilenm@gmail.com>
2024-02-05 21:46:48 -0300

    add account_income

--- cuenta_ingreso/__manifest__.py
+++ cuenta_ingreso/__manifest__.py
@@ -1,7 +1,12 @@
 {
     'name': 'income_account',
-    'description': """This module adds a field to the contact for income account
-     and changes the default on invoice lines.""",
+    'description': """This module adds a field to the contact for income account and changes the default on invoice lines.
+
+    Features:
+    - Introduces a new field for income account on contacts.
+    - Modifies the default behavior of invoice lines.
+
+    Price: $120.00""",
     'author': 'Numilen Monzon Soledad',
     'version': '1.0',
     'depends': ['base', 'account', 'contacts'],
@@ -13,3 +18,4 @@
     'auto_install': False,
     'license': 'LGPL-3',
 }
+
