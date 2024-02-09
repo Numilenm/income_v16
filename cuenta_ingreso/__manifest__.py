e8c5ed7e98a63a79d2d90a389dce2751403b0aae
Numilen <numilenm@gmail.com>
2024-02-08 21:59:44 -0300

    add update sale_order_line

--- cuenta_ingreso/__manifest__.py
+++ cuenta_ingreso/__manifest__.py
@@ -3,6 +3,7 @@
     'description': """This module adds a field to contacts for income account and changes the default on invoice lines. It also extends to sales order lines.""",
     'author': 'Numilen Monzon Soledad',
     'price': 250.00,
+    'currency': 'USD',
     'version': '1.0',
     'depends': ['base', 'account', 'contacts', 'sale'],
     'data': [
